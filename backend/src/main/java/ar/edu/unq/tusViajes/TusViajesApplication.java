ac4ad5b5ff7880405393addf45cbfb99404da831
Sebastian Correa <sebastiancorreaemanuel@gmail.com>
2026-08-25 17:42:36 -0300

    add maven and postgresql

---
+++ backend/src/main/java/ar/edu/unq/tusViajes/TusViajesApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.tusViajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TusViajesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ar/edu/unq/tusViajes/TusViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.tusViajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
