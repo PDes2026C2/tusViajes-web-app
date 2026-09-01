5fd7f704d59fd8203828ea84e1689514bd7bc14e
thiago.sofarelli <sofarelli.thiago10@gmail.com>
2026-09-01 10:37:01 -0300

    Archivos de config. de docker

--- backend/src/main/java/ar/edu/unq/tusViajes/TusViajesApplication.java
+++ backend/src/main/java/ar/edu/unq/tusViajes/TusViajesApplication.java
@@ -3,10 +3,13 @@ package ar.edu.unq.tusViajes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TusViajesApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Argentina/Buenos_Aires"));
 		SpringApplication.run(TusViajesApplication.class, args);
 	}
 
